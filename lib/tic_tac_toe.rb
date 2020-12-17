3d241ce711cc47e44babda35326e1fac571d463a
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-17 13:44:57 -0600

    commit tree a882574762031f211d6fa5478871d603e7dcad94

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,98 @@
+require 'pry'
+class TicTacToe
+
+    WIN_COMBINATIONS = [ 
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    
+    def initialize
+        @board = Array.new(9, " ")
+    end
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index(input)
+        input.to_i - 1
+    end
+
+    def move(index, token = "X")
+        @board[index] = token
+    end
+
+    def position_taken?(index)
+        @board[index] != " "
+    end
+
+    def valid_move?(index)
+        index.between?(0,8) && !position_taken?(index)
+    end
+
+    def turn_count
+        @board.count {|position| position == "X" || position == "O"}
+    end
+
+    def current_player
+        turn_count.even? ? "X" : "O"
+    end
+
+    def turn
+        puts "Please enter a number between 1 and 9."
+        input = gets.strip
+        index = input_to_index(input)
+
+        if valid_move?(index)
+            move(index, current_player)
+            display_board
+        else
+            turn
+        end
+    end
+
+    def won?
+        WIN_COMBINATIONS.detect do |combo|
+            combo.all? {|i| @board[i] == "X"} || combo.all? {|i| @board[i] == "O"}
+        end
+    end
+
+    def full?
+        @board.all? {|x| x == "X" || x == "O"}
+    end
+
+    def draw?
+        full? && !won?
+    end
+
+    def over?
+        won? || draw?
+    end
+
+    def winner
+        @board[won?[0]] if won?
+    end
+    
+
+    def play
+        until over? do
+            turn
+        end
+        
+        if won?
+            puts "Congratulations #{winner}!"
+        else 
+            puts "Cat's Game!"
+        end
+    end
+end
